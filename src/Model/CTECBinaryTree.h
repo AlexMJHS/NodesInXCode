ef32fec0058e5464a22977fdbde2f4c3de7f5ce7
Alejandro <AlexMJHS@gmail.com>
2016-04-19 09:20:34 -0600

    Added prototypes for leftmost and rightmost child of subtrees

--- src/Model/CTECBinaryTree.h
+++ src/Model/CTECBinaryTree.h
@@ -22,6 +22,8 @@ private:
     bool contains(Type value, CTECBinaryTree<Type> * currentTree);
     void calculateSize(TreeNode<Type> * currentNode);
     void insert(const Type& value, CTECBinaryTree<Type> * currentTree);
+    TreeNode<Type> * getRightMostChild(CTECBinaryTree * leftSubTree);
+    TreeNode<Type> * getLeftMostChild(CTECBinaryTree * rightSubdTree);
     
 public:
     CTECBinaryTree();
